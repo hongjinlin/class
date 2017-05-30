3bbe4523725a164e5af78390a7fe0a7672eae534
hongjl <89757630@qq.com>
2017-05-30 21:23:43 +0800

    reply msg test

--- application/models/Message.php
+++ application/models/Message.php
@@ -1,6 +1,5 @@
 <?php if (!defined('APP_PATH')) exit('No direct script access allowed');
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
+
 class MessageModel extends Model
 {
     public function __construct()
@@ -12,16 +11,12 @@ class MessageModel extends Model
     {
         $server = $this->app->server;
 
-        $logger = new Logger('wx_msg');
-
-        $logger->pushHandler(new StreamHandler(__DIR__.'/../wx_msg_' . date('Y-m-d') . '.log', Logger::DEBUG));
-
-        $logger->info('msg', $message);
-        
         $server->setMessageHandler(function ($message) {
             // $message->FromUserName // 用户的 openid
             // $message->MsgType // 消息类型：event, text....
-            return "您好！欢迎关注我!";
+            $message->MsgType = 'image';
+            $message->PicUrl = "http://class.xmtopnews.com/images/class.jpeg";
+//            return "您好！欢迎关注我!";
         });
         $response = $server->serve();
         $response->send();
