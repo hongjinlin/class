5f621d9f69ef48f5b9addd56dd123b49c46bb65e
hongjl <89757630@qq.com>
2017-05-30 21:03:30 +0800

    add msg logs

--- application/controllers/Message.php
+++ application/controllers/Message.php
@@ -1,4 +1,5 @@
 <?php if ( ! defined('APP_PATH')) exit('No direct script access allowed');
+
 class MessageController extends Controller {
     public function indexAction(){
         $mMessage = new MessageModel();
--- application/models/Message.php
+++ application/models/Message.php
@@ -1,5 +1,6 @@
 <?php if (!defined('APP_PATH')) exit('No direct script access allowed');
-
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 class MessageModel extends Model
 {
     public function __construct()
@@ -10,6 +11,13 @@ class MessageModel extends Model
     public function response()
     {
         $server = $this->app->server;
+
+        $logger = new Logger('wx_msg');
+
+        $logger->pushHandler(new StreamHandler(__DIR__.'/../wx_msg_' . date('Y-m-d') . '.log', Logger::DEBUG));
+
+        $logger->info('msg', $message);
+        
         $server->setMessageHandler(function ($message) {
             // $message->FromUserName // 用户的 openid
             // $message->MsgType // 消息类型：event, text....
